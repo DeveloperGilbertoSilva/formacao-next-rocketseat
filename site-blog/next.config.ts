953f77e8b2b0bba07fe7883386f31890e6e15fff
Gilberto Silva <contatogilbertolopes@outlook.com.br>
2026-03-17 09:06:15 +0000

    Initial setup and feature section

---
+++ site-blog/next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  allowedDevOrigins: [
+    "localhost",
+    "127.0.0.1",
+    "localhost:3000",
+    "127.0.0.1:3000",
+  ],
+};
+
+export default nextConfig;
